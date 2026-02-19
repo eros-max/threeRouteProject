36657e022bb6249a018ac48b2c8456b769afc63d
Eros Max <erosmsc2@gmail.com>
2026-02-19 08:34:00 -0300

    Download de basicos e tal

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/itens', (req, res) => {
+    
+})
+
+app.post('/itens', (req, res) => {
+
+})
+
+app.delete('/itens', (req, res) => {
+
+})
+
+app.listen(3333, () => {
+    console.log('Servidor rodando na porta 3333.')
+})
